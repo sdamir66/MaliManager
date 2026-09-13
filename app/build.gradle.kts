cdae296b05459b921bf75dac9028cced36b9bbf3
sdamir66 <sdamir66@gmail.com>
2026-09-13 18:55:14 +0330

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+android { namespace="com.example.maliplus"; compileSdk=36
+    defaultConfig { applicationId="com.example.maliplus"; minSdk=24; targetSdk=36; versionCode=1; versionName="1.0" }
+}
+
+kotlin { jvmToolchain(17) }
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+    implementation("androidx.room:room-runtime:2.7.2")
+    implementation("androidx.room:room-ktx:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("io.github.faridsolgi:persianDateTime:1.0.0")
+}
