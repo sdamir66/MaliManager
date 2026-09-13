e0bb0b6069b8031632739c0c16f866a5fdb1f6e1
sdamir66 <sdamir66@gmail.com>
2026-09-13 20:24:45 +0330

    Create AppDb.kt

---
+++ app/src/main/java/com/example/maliplus/data/AppDb.kt
@@ -0,0 +1,24 @@
+package com.example.maliplus.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [
+        Account::class,
+        Transaction::class,
+        Good::class,
+        GoodTransaction::class,
+        ProfitSettings::class,
+        MonthlyRate::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDb : RoomDatabase() {
+    abstract fun accounts(): AccountDao
+    abstract fun tx(): TxDao
+    abstract fun goods(): GoodDao
+    abstract fun goodTx(): GoodTxDao
+    abstract fun profit(): ProfitDao
+}
