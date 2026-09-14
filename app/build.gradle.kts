4d230e4680d04f82a7a02f335825d541f0d8ffff
sdamir66 <sdamir66@gmail.com>
2026-09-14 22:05:37 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.documentfile:documentfile:1.0.1")
 
     // Room — هر سه روی 2.8.4
     implementation("androidx.room:room-runtime:2.8.4")
