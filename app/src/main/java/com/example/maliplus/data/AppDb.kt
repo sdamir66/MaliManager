5696b6807568bea9b66a89f5af382970bd1177fb
sdamir66 <sdamir66@gmail.com>
2026-09-18 23:16:20 +0330

    Update AppDb.kt

--- app/src/main/java/com/example/maliplus/data/AppDb.kt
+++ app/src/main/java/com/example/maliplus/data/AppDb.kt
@@ -8,19 +8,14 @@ import androidx.room.RoomDatabase
         Person::class,
         Account::class,
         Transaction::class,
-        ProfitSettings::class,
-        MonthlyRate::class,
-        GlobalProfitSettings::class,
-        GlobalMonthlyRate::class
+        ProfitPeriod::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class AppDb : RoomDatabase() {
     abstract fun persons(): PersonDao
     abstract fun accounts(): AccountDao
     abstract fun tx(): TxDao
-    abstract fun profit(): ProfitDao
-    abstract fun globalProfit(): GlobalProfitDao
-    abstract fun globalMonthlyRate(): GlobalMonthlyRateDao
+    abstract fun profitPeriod(): ProfitPeriodDao
 }
