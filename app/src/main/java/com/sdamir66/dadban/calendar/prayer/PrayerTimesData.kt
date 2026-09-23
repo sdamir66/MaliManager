c88f30f86f0525ea02d845dec1cb1b758ef66ba5
sdamir66 <sdamir66@gmail.com>
2026-09-24 01:19:04 +0330

    Update PrayerTimesData.kt

--- app/src/main/java/com/sdamir66/dadban/calendar/prayer/PrayerTimesData.kt
+++ app/src/main/java/com/sdamir66/dadban/calendar/prayer/PrayerTimesData.kt
@@ -1,9 +1,14 @@
+package com.sdamir66.dadban.calendar.prayer
+
+/**
+ * اوقات شرعی یک روز
+ */
 data class PrayerTimesData(
     val fajr: String,        // اذان صبح
     val sunrise: String,     // طلوع آفتاب
     val dhuhr: String,       // اذان ظهر
     val asr: String,         // عصر
-    val maghrib: String,     // اذان مغرب (= غروب آفتاب در این روش)
+    val maghrib: String,     // اذان مغرب (با زاویه 4.5 درجه)
     val isha: String,        // اذان عشا
     val dateMillis: Long,
     val latitude: Double,
